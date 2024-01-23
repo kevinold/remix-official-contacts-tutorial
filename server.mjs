6931af61f447391276e0db4da5cf91d5fb891b69
Kevin Old <kevin@kevinold.com>
2024-01-23 11:46:04 -0600

    configure per ampt docs for remix

--- server.mjs
+++ server.mjs
@@ -1,23 +1,25 @@
-import { http } from "@ampt/sdk";
-import { createRequestHandler } from "@remix-run/express";
-//import { broadcastDevReady } from "@remix-run/node";
-import express from "express";
+import "@ampt/remix";
 
-// notice that the result of `remix build` is "just a module"
-import * as build from "./build/index.js";
+// import { http } from "@ampt/sdk";
+// import { createRequestHandler } from "@remix-run/express";
+// //import { broadcastDevReady } from "@remix-run/node";
+// import express from "express";
 
-const app = express();
-app.use(express.static("static"));
+// // notice that the result of `remix build` is "just a module"
+// import * as build from "./build/index.js";
 
-// and your app is "just a request handler"
-app.all("*", createRequestHandler({ build }));
+// const app = express();
+// app.use(express.static("static"));
 
-// app.listen(3000, () => {
-//   if (process.env.NODE_ENV === "development") {
-//     broadcastDevReady(build);
-//   }
-//   console.log("App listening on http://localhost:3000");
-// });
+// // and your app is "just a request handler"
+// app.all("*", createRequestHandler({ build }));
 
-http.node.use(app)
+// // app.listen(3000, () => {
+// //   if (process.env.NODE_ENV === "development") {
+// //     broadcastDevReady(build);
+// //   }
+// //   console.log("App listening on http://localhost:3000");
+// // });
+
+// http.node.use(app)
 
